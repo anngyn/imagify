5baa21742b6e1e947c2e711ea55f08bb3a27d734
anngyn <nguyenan16032004@gmail.com>
2025-09-19 12:38:31 +0700

    create UI

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredits from './pages/BuyCredits'
+import Navbar from './componants/Navbar'
+import Footer from './componants/Footer'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to orange-50'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy' element={<BuyCredits />} />
+      </Routes>
+      <Footer />
+
+    </div>
+  )
+}
+
+export default App
